bf21ae1012f3322dbfba393bb925118db1850053
Ian Reyes <156475427+ianreyess@users.noreply.github.com>
2024-02-09 15:48:48 -0600

    Parcial_01

--- Proyecto01_Ian/Assets/Scripts/InventorManager.cs
+++ Proyecto01_Ian/Assets/Scripts/InventorManager.cs
@@ -5,37 +5,36 @@ using UnityEngine.UI;
 
 public class InventorManager : MonoBehaviour
 {
-   public static InventorManager Instance;
-   public List<Item> Items = new List<Item>();
-   public Transform ItemContent;
-   public GameObject InventoryItem;
+    public static InventorManager Instance;
+    public List<Item> Items = new List<Item>();
+    public Transform ItemContent;
+    public GameObject InventoryItem;
 
-   private void Awake()
-   {
-       Instance = this;
-   }
+    private void Awake()
+    {
+        Instance = this;
+    }
 
-   public void Add(Item item)
-   {
-       Items.Add(item);
-   }
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
 
-   public void Remove(Item item)
-   {
-       Items.Remove(item);
-   }
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
 
-   public void ListItems()
-   {
-       foreach (var item in Items)
-       {
-           GameObject obj = Instantiate(InventoryItem, ItemContent);
-           var itemName = obj.transform.Find("itemName").GetComponent<Text>();
-           var itemIcon = obj.transform.Find("itemIcon").GetComponent<Image>();
+    public void ListItems()
+    {
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<Text>(); 
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>(); 
 
-           
-           itemName.text = item.ItemName;
-           itemIcon.sprite = item.icon;
-       }
-   }
+            itemName.text = item.ItemName;
+            itemIcon.sprite = item.icon;
+        }
+    }
 }
